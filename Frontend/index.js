d55d2497e0f813fe770b772ac609ed4054cf4f6e
Dexter Rowland <dexter.rowland@outlook.com>
2026-02-09 23:27:18 +0000

    playig around with frontend index.js, no actual changes

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,12 @@
 const app = require('express')();
 
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Backend running');
+// });
+
+
 app.get('/', (req, res) =>
     res.json({ message: 'Welcome to Urban Botany!' })
 );
