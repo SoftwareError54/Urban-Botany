a594bd887e14d168328a8c74a722cb6ccb57de59
Dexter <dexter.rowland@outlook.com>
2026-02-12 12:46:54 +0000

    Change the routing for backend API architecture

--- Backend/Services/AuthService.js
+++ Backend/Services/AuthService.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import {findUserByEmail, createUser} from '../Entities/User.js';
+import {findUserByEmail, createUser} from '../Repository/AuthRepository.js';
 
 const saltRounds = 10;
 const JWT_SECRET = process.env.JWT_SECRET
