9144dd9bdca5704ed3a08e666a87e0fd51552727
Dexter <dexter.rowland@outlook.com>
2026-02-13 10:50:43 +0000

    installing mysql2, and setting uo db.js in config

---
+++ Backend/config/db.js
@@ -0,0 +1,27 @@
+createRequire("dotenv").config();
+
+const mysql = require('mysql2/promise');
+
+const config = {
+    db: { /* do not put password or any sensitive info here, done only for demo */
+    host: process.env.MYSQL_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.MYSQL_ROOT_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 2,
+    queueLimit: 0,
+  },
+};
+
+const pool = mysql.createPool(config.db);
+
+async function query (sql, params){
+    const [rows, fields] = await pool.execute(sql, params);
+    return rows;
+}
+
+module.exports = {
+    query,
+}
