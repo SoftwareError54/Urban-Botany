7928faa13d4fbf8b003c4ee4d35520215dfee756
Dexter <dexter.rowland@outlook.com>
2026-04-22 13:39:50 +0100

    case mis-matching for railway deployment, updated backend/routes/plantRoutes import statements.

--- Backend/Routes/PlantRoutes.js
+++ Backend/Routes/PlantRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as plantController from '../Controllers/plantController.js';
+import * as plantController from '../Controllers/PlantController.js';
 import {authenticateToken} from '../Middleware/authMiddleware.js';
 
 const router = express.Router();
--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,8 +2,19 @@ import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
 dotenv.config();
 
-const config = {
-    db: {
+// const config = {
+//     db: {
+//     host: process.env.MYSQL_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.MYSQL_ROOT_USER,
+//     password: process.env.MYSQL_ROOT_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+//     waitForConnections: true,
+//     connectionLimit: 2,
+//     queueLimit: 0,
+//   },
+// };
+const config = process.env.MYSQL_URL || process.env.DATABASE_URL || {
     host: process.env.MYSQL_HOST,
     port: process.env.DB_PORT,
     user: process.env.MYSQL_ROOT_USER,
@@ -12,10 +23,9 @@ const config = {
     waitForConnections: true,
     connectionLimit: 2,
     queueLimit: 0,
-  },
 };
 
-const pool = mysql.createPool(config.db);
+const pool = mysql.createPool(config);
 
 export async function query (sql, params){
     const [rows, fields] = await pool.execute(sql, params);
