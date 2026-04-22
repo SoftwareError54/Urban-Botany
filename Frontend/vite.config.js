672d4170e9fd91e0334e0a430ffae2e6128428e7
Dexter <dexter.rowland@outlook.com>
2026-04-22 13:54:55 +0100

    adding server to allowed lists

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['urban-botany-production.up.railway.app']
+  }
 })
-
-server:{
-  allowedHosts: ['urban-botany-production.up.railway.app']
-}
