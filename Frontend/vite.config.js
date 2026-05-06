7e5487af67f2a66861bda297428efef8c9c58cca
Dexter <dexter.rowland@outlook.com>
2026-05-06 11:49:33 +0100

    changing url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['urban-botany-production.up.railway.app']
+    allowedHosts: ['urban-botany.up.railway.app']
   }
 })
